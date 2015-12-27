96a8c4c645677296f457bc4d2893581a88af8531
hongjong <max96min@naver.com>
2015-12-27 09:08:24 +0900

    dynamic page test

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function(req,res){
-  res.send('Hello world!');
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,'public')));
+
+var data={count:0};
+app.get('/',function (req,res) {
+  data.count++;
+  res.render('my_first_ejs',data);
 });
+app.get('/reset',function (req,res) {
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count',function (req,res) {
+  data.count++;
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num',function (req,res) {
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
+
+//app.use(express.static(__dirname+'/public'));
+//console.log(__dirname);
+//console.log(__dirname);
+/*app.get('/',function(req,res){
+  res.send('Hello world!');
+});*/
 
 app.listen(3000,function(){
-  console.log('Server On!');
+  console.log('Server On 2!');
 });
-
-  
-
-
-/* function fizzBuzz(argument) {
-  for(i=1;i<=100;i++){
-    console.log("hello : "+i);
-  }
-}
-
-fizzBuzz();
-*/
